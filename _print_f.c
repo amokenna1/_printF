8d7a17f47c0e66fcfa1cbce9cb94d4ac2664b080
amokenna1 <amokenna@icloud.com>
2023-11-11 03:31:06 -0800

    _print_f.c

---
+++ _print_f.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+int _printf(const char *format, ....)
+{
+	int chk_print = 0;
+
+	va_list args_lst;
+
+	if(format == NULL)
+	{
+		return (-1);
+	}
+
+	va_start(args_lst, format);
+
+	while(*format)
+	{
+		if 
+	}
+	
+	va_arg
+
+	va_end
