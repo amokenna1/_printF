b03b2ef84b07c2d6c2232e8d27db6f2b35d1ce39
Miracle <amokenna@icloud.com>
2023-11-13 10:25:47 +0100

    modified:   _print_f.c 	modified:   main.h
    Task 0

--- _print_f.c
+++ _print_f.c
@@ -1,24 +1,59 @@
 #include "main.h"
 
-int _printf(const char *format, ....)
+int _printf(const char *format, ...)
 {
-	int chk_print = 0;
+	int chr_Print = 0;
 
 	va_list args_lst;
 
 	if(format == NULL)
-	{
 		return (-1);
-	}
 
 	va_start(args_lst, format);
 
 	while(*format)
 	{
-		if 
-	}
-	
-	va_arg
+		if(*format != '%')
+		{
+			write(1, format, 1);
+			chr_Print++;
+		}
+		else 
+		{
+			format++; 
+			if (*format =='\0')
+				break;
+			
+			if(*format == '%')
+			{
+				write(1, format, 1);
+				chr_Print++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(args_lst, int);
+				write(1, &c, 1);
+				chr_Print++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args_lst, char*);
+				int str_length = strlen(str);
 
-	va_end
+				write(1, str, str_length);
+				chr_Print += str_length;
+			}
+		}
+		
+		format++;
+	}
+
+	va_end (args_lst);
+
+	return chr_Print;
+}
+
+int main()
+{
+	printf("Miracle\0");
 }
