ad710152d1e6c79729182b7433168f52182b60b1
Miracle <amokenna@icloud.com>
2023-11-15 10:15:51 +0100

    modified:   .vscode/c_cpp_properties.json 	new file:   .vscode/launch.json modified:   .vscode/settings.json 	modified:   main.h

--- main.h
+++ main.h
@@ -1,13 +1,33 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <string.h>
 #include <unistd.h>
 
-
+/**
+ * _printf - Custom printf function
+ * @format: Format specifier
+ * 
+ * Return: Number of characters printed (excluding null byte)
+ */
 int _printf(const char *format, ...);
-int binary (int num);
+
+/**
+ * binary - Convert a decimal number to binary
+ * @num: Decimal number to convert
+ * 
+ * Return: Binary representation
+ */
+int binary(int num);
+
+/**
+ * octal - Convert a decimal number to octal
+ * @num: Decimal number to convert
+ * 
+ * Return: Octal representation
+ */
 int octal(int num);
 
-
 #endif /* MAIN_H */
+
